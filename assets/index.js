86fdcabd8315275aecfab86014284130e2198e37
ZbyszekToKrol <siataturniej@gmail.com>
2025-11-18 18:29:37 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -69,40 +69,81 @@ imageInput.addEventListener('change', async () => {
 });
 // === END IMAGE UPLOAD REPLACEMENT ===
 
-  document.querySelectorAll(".input_holder").forEach((element) => {
+document.querySelectorAll(".input_holder").forEach((element) => {
+
     var input = element.querySelector(".input");
+    input.addEventListener('click', () => {
+        element.classList.remove("error_shown");
+    })
 
-    if (isEmpty(input.value)) {
-      empty.push(element);
-      element.classList.add("error_shown");
-    } else {
-      params.set(input.id, input.value);
+});
+
+document.querySelector(".go").addEventListener('click', () => {
+
+    var empty = [];
+
+    var params = new URLSearchParams();
+
+    params.set("sex", sex)
+    if (!upload.hasAttribute("selected")){
+        empty.push(upload);
+        upload.classList.add("error_shown")
+    }else{
+        params.set("image", upload.getAttribute("selected"))
     }
-  });
 
-  if (empty.length != 0) {
-    empty[0].scrollIntoView();
-  } else {
-    forwardToId(params);
-  };
+    var birthday = "";
+    var dateEmpty = false;
+    document.querySelectorAll(".date_input").forEach((element) => {
+        birthday = birthday + "." + element.value
+        if (/^\s*$/.test(element.value)){
+            dateEmpty = true;
+        }
+    })
 
-function isEmpty(value) {
-  let pattern = /^\s*$/;
-  return pattern.test(value);
-}
+    birthday = birthday.substring(1);
 
-function forwardToId(params) {
-  location.href = "/id?" + params;
+    if (dateEmpty){
+        var dateElement = document.querySelector(".date");
+        dateElement.classList.add("error_shown");
+        empty.push(dateElement);
+    }else{
+        params.set("birthday", birthday)
+    }
+
+    document.querySelectorAll(".input_holder").forEach((element) => {
+
+        var input = element.querySelector(".input");
+
+        if (/^\s*$/.test(input.value)){
+            empty.push(element);
+            element.classList.add("error_shown");
+        }else{
+            params.set(input.id, input.value)
+        }
+
+    })
+
+    if (empty.length != 0){
+        empty[0].scrollIntoView();
+    }else{
+
+        forwardToId(params);
+    }
+
+});
+
+function forwardToId(params){
+
+location.href = "/szpeloza/id?" + params
+
+
 }
 
 var guide = document.querySelector(".guide_holder");
-guide.addEventListener("click", () => {
-  if (guide.classList.contains("unfolded")) {
-    guide.classList.remove("unfolded");
-  } else {
-    guide.classList.add("unfolded");
-  }
-});
+guide.addEventListener('click', () => {
 
+    guide.classList.toggle("unfolded");
 
+})
 
